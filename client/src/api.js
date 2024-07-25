1193f70867d5896e8463276e6519b35431e34e30
Dani672002 <146192445+Dani672002@users.noreply.github.com>
2024-07-26 02:14:40 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,1 @@
 
-import axios from 'axios';
-import config from './config';
-
-const apiClient = axios.create({
-  baseURL: config.backendUrl,
-});
-
-export const getEmployees = () => apiClient.get('/employees');
-export const getEmployeeById = (id) => apiClient.get(`/employees/${id}`);
-export const createEmployee = (employeeData) => apiClient.post('/employees', employeeData);
-export const updateEmployee = (id, employeeData) => apiClient.put(`/employees/${id}`, employeeData);
-export const deleteEmployee = (id) => apiClient.delete(`/employees/${id}`);
