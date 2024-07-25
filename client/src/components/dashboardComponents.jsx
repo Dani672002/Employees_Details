9c1520e8eced30b4a8632f33b3b45c15f57e6dd4
Dani672002 <146192445+Dani672002@users.noreply.github.com>
2024-07-26 02:15:26 +0530

    Update dashboardComponents.jsx

--- client/src/components/dashboardComponents.jsx
+++ client/src/components/dashboardComponents.jsx
@@ -5,27 +5,15 @@ import Button from "@mui/material/Button";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { Link, useNavigate } from "react-router-dom";
 import AddIcon from "@mui/icons-material/Add";
-import { useLocation } from "react-router-dom";
-import axios from "axios";
-import config from "../config"; // Import the configuration
+import { useLocation } from 'react-router-dom';
 
 const DashboardComponents = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-  const navigate = useNavigate();
+
+  // username methods//
   const location = useLocation();
   const { username } = location.state || {};
-
-  // Example function to handle logout
-  const handleLogout = async () => {
-    try {
-      await axios.post(`${config.backendUrl}/auth/logout`); // Assuming there's a logout endpoint
-      navigate("/"); // Navigate to the login page after logging out
-    } catch (error) {
-      console.error("Logout failed:", error);
-    }
-  };
-
   return (
     <>
       <Box sx={{ backgroundColor: "#000", minHeight: "100vh" }}>
@@ -45,118 +33,123 @@ const DashboardComponents = () => {
           />
         </Box>
         <Container>
-          <Box
-            style={{
-              backgroundColor: "#121416",
-              width: "100%",
-              padding: "10px",
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <Button
-              style={{
-                backgroundColor: "#000",
-                color: "white",
-                border: "1px solid black",
-                borderRadius: "10px",
-                width: "100px",
-                marginRight: "10px",
-              }}
-            >
-              Home
-            </Button>
-            <Link
-              to="/list"
-              style={{
-                textDecoration: "none",
-                backgroundColor: "black",
-                color: "white",
-                border: "1px solid black",
-                borderRadius: "10px",
-                marginRight: "10px",
-              }}
-            >
-              <Button
-                style={{
-                  backgroundColor: "black",
-                  color: "white",
-                  border: "1px solid black",
-                  borderRadius: "10px",
-                  width: "100px",
-                }}
-              >
-                Employee List
-              </Button>
-            </Link>
-            <Button
-              style={{
-                backgroundColor: "black",
-                color: "white",
-                border: "1px solid black",
-                borderRadius: "10px",
-                width: "100px",
-                marginRight: "10px",
-              }}
-            >
-              {username || "User"}
-            </Button>
-            <Link
-              to="/"
-              style={{
-                textDecoration: "none",
-                backgroundColor: "black",
-                color: "white",
-                border: "1px solid black",
-                borderRadius: "10px",
-              }}
-              onClick={handleLogout}
-            >
-              <Button style={{ paddingTop: "20px" }}>
-                <LogoutIcon
-                  style={{
-                    color: "white",
+      <Box
+        style={{
+          backgroundColor: "#121416",
+          width: "100%",
+          padding: "10px",
+          display: "flex",
+          justifyContent: "space-between",
                   }}
-                />
-              </Button>
-            </Link>
-          </Box>
+      >
+        <Button
+          style={{
+            backgroundColor: "#000",
+            color: "white",
+            border: "1px solid black",
+            borderRadius: "10px",
+            width: "100px",
+            marginRight:'10px'
 
-          <Box
+          }}
+        >
+          Home
+        </Button>
+        <Link
+          to="/list"
+          style={{
+            textDecoration: 'none',
+            backgroundColor: "black",
+            color: "white",
+            border: "1px solid black",
+            borderRadius: "10px",
+            marginRight:'10px'
+
+          }}
+        >
+          <Button
             style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              padding: "10px",
-              marginTop: "10px",
+              backgroundColor: "black",
+              color: "white",
+              border: "1px solid black",
+              borderRadius: "10px",
+              width: "100px",
+
             }}
           >
-            <Link to="/employee" style={{ textDecoration: "none" }}>
-              <Button
-                style={{
-                  backgroundColor: "#2196f3",
-                  color: "black",
-                  border: "1px solid black",
-                  borderRadius: "10px",
-                  height: "100%",
-                }}
-              >
-                Create Employee
-                <AddIcon style={{ fontSize: "30px" }} />
-              </Button>
-            </Link>
-          </Box>
+            Employee List
+          </Button>
+        </Link>
+        <Button
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            border: "1px solid black",
+            borderRadius: "10px",
+            width: "100px",
+            marginRight:'10px'
 
-          <Box
+          }}
+        >
+          {username || "User"}
+        </Button>
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+            backgroundColor: "black",
+            color: "white",
+            border: "1px solid black",
+            borderRadius: "10px",
+          }}
+        >
+          <Button style={{   paddingTop:'20px'}}>
+            <LogoutIcon
+              style={{
+                color: "white",
+                
+              }}
+            />
+          </Button>
+        </Link>
+      </Box>
+
+      <Box
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          padding: "10px",
+          marginTop: "10px", // Adjust margin-top based on the height of your fixed header
+        }}
+      >
+        <Link to="/employee" style={{ textDecoration: 'none' }}>
+          <Button
             style={{
-              alignItems: "center",
-              marginTop: "120px",
+              backgroundColor: "#2196f3",
+              color: "black",
+              border: "1px solid black",
+              borderRadius: "10px",
+              height: "100%",
             }}
           >
-            <h1 style={{ color: "white", fontWeight: "800", textAlign: "center" }}>
-              Welcome {username || "User"}
-            </h1>
-          </Box>
-        </Container>
+            Create Employee
+            <AddIcon style={{ fontSize: "30px" }} />
+          </Button>
+        </Link>
+      </Box>
+
+      <Box
+        style={{
+          
+          alignItems: "center",
+          marginTop:"120px"
+        }}
+      >
+        <h1 style={{ color: "white", fontWeight: "800", textAlign: "center" }}>
+          Welcome {username || 'User'}
+        </h1>
+      </Box>
+    </Container>
       </Box>
     </>
   );
